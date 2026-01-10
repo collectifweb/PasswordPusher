e927c633230ab4330b892acb0a87ee7d205ef05c
Collectif WEB <alexandre@collectifweb.Ca>
2026-01-10 00:59:58 -0500

    Allow secrets.collectifweb.ca

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,8 +121,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
+   config.hosts = [
+     "secrets.collectifweb.ca",     
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
